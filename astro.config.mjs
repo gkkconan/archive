66dd5b179922c67e6dccf28a4c254c8ada23e55a
gkkconan <conan.gkk@gmail.com>
2024-06-04 21:23:12 +0200

    fix: update github pages link

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://gkkconan.github.io',
+	site: 'https://gkkconan.github.io/archive/',
 	integrations: [mdx(), sitemap()],
 });
